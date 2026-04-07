6e65b3607501edb40a30a87789043f7d0368a27c
kikuchimkt-afk <kikuchi.mkt@gmail.com>
2026-04-07 15:04:14 +0900

    up

--- src/app/s/[token]/page.js
+++ src/app/s/[token]/page.js
@@ -1,12 +1,17 @@
 import StudentDashboard from './StudentDashboard';
-import StudentManifestInjector from './StudentManifestInjector';
+
+export async function generateMetadata({ params }) {
+  const { token } = await params;
+  return {
+    manifest: `/api/manifest?token=${token}`,
+  };
+}
 
 export default async function StudentPage({ params }) {
   const { token } = await params;
 
   return (
     <div className="container" style={{ paddingTop: '1.5rem', paddingBottom: '3rem' }}>
-      <StudentManifestInjector />
       <StudentDashboard token={token} />
     </div>
   );
